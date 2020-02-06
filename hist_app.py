86c5490704e1277139d9b361c12f075cec9d4529
Sarrion <sarrionmas.a@gmail.com>
2020-02-06 19:06:38 +0000

    hist replaced by bar chart, mean and main quantile lines added

--- hist_app.py
+++ hist_app.py
@@ -8,6 +8,8 @@ import dropdown_options
 import dash_bootstrap_components as dbc
 from dash_bootstrap_components import Col, Row
 from numpy import linspace
+import numpy as np
+import plotly.graph_objects as go
 
 df=pd.read_csv('/merged_perfiles_fondos.csv')
 var = [{'label':i, 'value':i} for i in df.filter(regex='^[^Q]').columns]
@@ -17,35 +19,41 @@ def dropdown(ide, options):
     return dcc.Dropdown(id=ide, options = options, value=options[0]['value'])
 
 def checklist(ide, options):
-    return dcc.Checklist(id=ide, options = options, value=[options[0]['value']], labelStyle={'display':'inline-block'})
+    return dcc.Checklist(id=ide, options = options, value=[x['value'] for x in options],
+                         labelStyle={'display':'inline-block'})
     
 def rgslider(ide, var, n_marks):
     limits = (var.min(), var.max())
     return dcc.RangeSlider(id = ide, min=limits[0], max=limits[1], 
                            step=(limits[1]-limits[0])/100, value = limits,
-                           marks = {i:'{}'.format(i) for i in linspace(limits[0], limits[1], n_marks)})
+                           marks = {i:'{:.1f}'.format(i) for i in linspace(limits[0], limits[1], n_marks)})
 
 
 
 
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.CERULEAN])
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
 
 layout = {
     "autosize":True,
     "automargin":True,
     "margin":{"l":"30", "r":"30", "b":"50", "t":"80"},
-    #"paper_bgcolor": "rgba(220, 220, 220, 0.8)",
-    #"plot_bgcolor": "rgba(1, 1, 1, 0.1)",
-    #"titlefont": {"color": "#FFF"},
-    #"xaxis": {"tickfont": {"color": "#FFF"}},
-    #"yaxis": {"tickfont": {"color": "#FFF"}}
+    "paper_bgcolor": "#555",
+    "plot_bgcolor": "rgba(1, 1, 1, 0.1)",
+    "titlefont": {"color": "#FFF"},
+    "xaxis": {"tickfont": {"color": "#FFF"}},
+    "yaxis": {"tickfont": {"color": "#FFF"}}
 }
 
 app.layout = html.Div([
     Row([
+        Col([html.H1('Histogram Comparison for bme data',
+                     style = {'margin-bottom':50, 'margin-top':20})],
+            width={"size": 12, "offset": 1})
+    ]),
+    Row([
         Col([
             dcc.Graph(id='hist_1', style={'width':'95%','box-shadow':'2px 2px 2px grey'})
-        ], width={"size": 3, "offset": 1}),
+        ], width={"size": 4, "offset": 1}),
         Col([
             html.Div([
                 html.Div([
@@ -57,11 +65,11 @@ app.layout = html.Div([
                     Row([Col(html.H5('Q3: '), sm=1), Col(checklist('multi_Q3_1',dropdown_options.Q3))]),
                     Row([Col(html.H5('Q4: '), sm=1), Col(rgslider('rgslider_Q4_1',df.Q4, 8))]),
                     Row([Col(html.H5('Q5: '), sm=1), Col(rgslider('rgslider_Q5_1',df.Q5, 5))]),
-                    Row([Col(html.H5('Q6: '), sm=1), Col(rgslider('rgslider_Q6_1',df.Q6, 3))]),
+                    Row([Col(html.H5('Q6: '), sm=1), Col(rgslider('rgslider_Q6_1',df.Q6, 5))]),
                     Row([Col(html.H5('Q7: '), sm=1), Col(checklist('multi_Q7_1',dropdown_options.Q7))]),
-                    Row([Col(html.H5('Q8: '), sm=1), Col(rgslider('rgslider_Q8_1',df.Q8, 3))]),
+                    Row([Col(html.H5('Q8: '), sm=1), Col(rgslider('rgslider_Q8_1',df.Q8, 5))]),
                     Row([Col(html.H5('Q9: '), sm=1), Col(checklist('multi_Q9_1',dropdown_options.Q9))]),
-                    Row([Col(html.H5('Q10: '), sm=1), Col(rgslider('rgslider_Q10_1',df.Q10, 3))])
+                    Row([Col(html.H5('Q10: '), sm=1), Col(rgslider('rgslider_Q10_1',df.Q10, 5))])
                 ], style={'width':'95%'})
             ], style={'width':'95%', 'box-shadow':'2px 2px 2px grey'})
         ])
@@ -70,7 +78,7 @@ app.layout = html.Div([
     Row([
         Col([
             dcc.Graph(id='hist_2', style={'width':'95%','box-shadow':'2px 2px 2px grey'})
-        ], width={"size": 3, "offset": 1}),
+        ], width={"size": 4, "offset": 1}),
         Col([
             html.Div([
                 html.Div([
@@ -82,18 +90,18 @@ app.layout = html.Div([
                     Row([Col(html.H5('Q3: '), sm=1), Col(checklist('multi_Q3_2',dropdown_options.Q3))]),
                     Row([Col(html.H5('Q4: '), sm=1), Col(rgslider('rgslider_Q4_2',df.Q4, 8))]),
                     Row([Col(html.H5('Q5: '), sm=1), Col(rgslider('rgslider_Q5_2',df.Q5, 5))]),
-                    Row([Col(html.H5('Q6: '), sm=1), Col(rgslider('rgslider_Q6_2',df.Q6, 3))]),
+                    Row([Col(html.H5('Q6: '), sm=1), Col(rgslider('rgslider_Q6_2',df.Q6, 5))]),
                     Row([Col(html.H5('Q7: '), sm=1), Col(checklist('multi_Q7_2',dropdown_options.Q7))]),
-                    Row([Col(html.H5('Q8: '), sm=1), Col(rgslider('rgslider_Q8_2',df.Q8, 3))]),
+                    Row([Col(html.H5('Q8: '), sm=1), Col(rgslider('rgslider_Q8_2',df.Q8, 5))]),
                     Row([Col(html.H5('Q9: '), sm=1), Col(checklist('multi_Q9_2',dropdown_options.Q9))]),
-                    Row([Col(html.H5('Q10: '), sm=1), Col(rgslider('rgslider_Q10_2',df.Q10, 3))])
+                    Row([Col(html.H5('Q10: '), sm=1), Col(rgslider('rgslider_Q10_2',df.Q10, 5))])
                 ], style={'width':'95%'})
             ], style={'width':'95%','box-shadow':'2px 2px 2px grey'})
         ])
     ]),
     html.Div('bme-data')
         
-], style = {'background-color': 'rgba(1, 1, 1, 0.1)'})
+], style = {'background-color': '#333'})
 
 
 
@@ -172,14 +180,51 @@ def display_hist2(Q0_filter, Q1_filter, Q2_filter, Q3_filter, Q7_filter, Q9_filt
         
     layout['title'] = var
     
+    raw_values = df[mask][var].dropna()
+    n_bins = int(np.sqrt(raw_values.shape[0]))
+    x = np.histogram(raw_values, bins = n_bins)[1]
+    y = np.histogram(raw_values, bins = n_bins)[0]
+    
+    mean = raw_values.mean()
+    median = raw_values.median()
+    fst_quarter = raw_values.quantile(0.25)
+    thr_quarter = raw_values.quantile(0.75)
+    max_hist = y.max()
+    
     result = {
         'data': [
             {
-                'x': df[mask][var],
-                'type': 'histogram'
+                'x': x,
+                'y': y,
+                'type': 'bar'
+            },
+            {
+                'x': [mean, mean],
+                'y': [0, max_hist * 1.01],
+                'type': 'line', 
+                'name': 'mean',
+            },
+            {
+                'x': [median, median],
+                'y': [0, max_hist * 1.01],
+                'type': 'line', 
+                'name': 'median'
+            },
+            {
+                'x': [fst_quarter, fst_quarter],
+                'y': [0, max_hist * 0.7],
+                'type': 'line', 
+                'name': 'fst_quarter'
+            },
+            {
+                'x': [thr_quarter, thr_quarter],
+                'y': [0, max_hist * 0.7],
+                'type': 'line', 
+                'name': 'thr_quarter'
             },
         ],
-        'layout': layout
+        'layout': layout,
+        
     }
     
     return result
